c741cdc1e7ae2fb0f79c56462239bd711e9a6630
orion-kendrix <mishrashivesh729@gmail.com>
2026-02-01 10:47:43 +0530

    Limit random range and track player guesses
    Reduce random coordinate range from ±100 to ±10 and add guess history tracking. Introduces count and arrays (dataX, dataY) to record each guess when attempts are consumed and prints the collected coordinates at the end. Also fixes a few directional output symbols and updates Maven compiler input/created files (build metadata). These changes enable showing the player's past guesses and make the game operate within a smaller coordinate space.

--- src/main/java/com/mycompany/java26/Java26.java
+++ src/main/java/com/mycompany/java26/Java26.java
@@ -14,11 +14,13 @@ public class Java26 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        int randomX = ThreadLocalRandom.current().nextInt(-100, 101);
-        int randomY = ThreadLocalRandom.current().nextInt(-100, 101);
+        int randomX = ThreadLocalRandom.current().nextInt(-10, 11);
+        int randomY = ThreadLocalRandom.current().nextInt(-10, 11);
         int attempts = 0;
-
+        int count = -1;
+        int[] dataX = new int[100];
+        int[] dataY = new int[100];
+        
         System.out.println("Welcome to the game!");
         System.out.println("Choose the difficulty of the game(based on the difficulty you will get a certain amount attempts to win the game:\n1 1.Easy(20 attempts)\n2 2.Medium(15 attempts)\n3 3.Hard(10 attempts)"); //Difficulty Option
         int diff = Integer.parseInt(scan.nextLine());
@@ -42,34 +44,58 @@ public class Java26 {
             if ((guessX == randomX) && (guessY < randomY)) {
                 System.out.println("STOP!! go up");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessX == randomX) && (guessY > randomY)) {
                 System.out.println("STOP!! go down");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessY == randomY) && (guessX < randomX)) {
                 System.out.println("STOP! go straight forward");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessY == randomY) && (guessX > randomX)) {
                 System.out.println("STOP! go straight back");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessX < randomX) && (guessY < randomY)) {
                 System.out.println(">^");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessX < randomX) && (guessY > randomY)) {
                 System.out.println(">!");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessX > randomX) && (guessY < randomY)) {
-                System.out.println("^<");
+                System.out.println("<^");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessX > randomX) && (guessY > randomY)) {
-                System.out.println("!<");
+                System.out.println("<!");
                 attempts--;
+                count++;
+                dataX[count] = guessX;
+                dataY[count] = guessY;
                 System.out.println("Attempts left:  " + attempts);
             } else if ((guessX == randomX) && (guessY == randomY)) {
                 System.out.println("You win!! Hooray!!");
@@ -79,6 +105,11 @@ public class Java26 {
         } while (attempts > 0);
         if (attempts == 0) {
             System.out.println("You lose!");
+        }
+        
+        System.out.println("All of your coordinates: ");
+        for (int i = 0;i < count;i++) {
+            System.out.println(i + "(" + dataX[i] + "," + dataY[i] + ")");
         }
     }
 }
